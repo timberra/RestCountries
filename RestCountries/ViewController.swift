574ea8c0082c4510cbd2f14afba2b52d182abd69
timberra <145005326+timberra@users.noreply.github.com>
2023-11-16 15:47:30 +0200

    Initial Commit

---
+++ RestCountries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestCountries
+//
+//  Created by liga.griezne on 16/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
